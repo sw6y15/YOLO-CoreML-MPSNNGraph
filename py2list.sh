92c5614842992d2e5c61386f65ebe359df67035e
sw6y15 <sw6y15@soton.ac.uk>
2018-12-28 21:47:49 +0800

    Dependency installation

---
+++ py2list.sh
@@ -0,0 +1,10 @@
+apt-get update
+apt-get install -y python
+apt-get install python-pip
+pip install tensorflow
+pip install keras==1.2.2
+pip install h5py
+pip install coremltools
+pip install winmltools
+pip install onnxmltools
+pip list
---
+++ py3list.sh
@@ -0,0 +1,7 @@
+apt-get install -y --no-install-recommends python3-pip
+pip3 install tensorflow
+pip3 install keras==1.2.2
+pip3 install h5py
+pip3 install pydot-ng
+pip3 install pillow
+pip3 list
