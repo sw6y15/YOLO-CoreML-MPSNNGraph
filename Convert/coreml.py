a782b5859d0f2a1e5e162919596ab404fffcefe9
sw6y15 <sw6y15@soton.ac.uk>
2018-12-29 13:40:16 +0800

    Update coreml.py
    Change the convert model output name

--- Convert/coreml.py
+++ Convert/coreml.py
@@ -5,17 +5,17 @@ import coremltools
 
 coreml_model = coremltools.converters.keras.convert(
     'yad2k/model_data/tiny-yolo-voc.h5',
-    input_names='image',
-    image_input_names='image',
-    output_names='grid',
+    input_names='data',
+    image_input_names='data',
+    output_names='model_outputs0',
     image_scale=1/255.)
 
 coreml_model.author = 'Original paper: Joseph Redmon, Ali Farhadi'
 coreml_model.license = 'Public Domain'
 coreml_model.short_description = "The Tiny YOLO network from the paper 'YOLO9000: Better, Faster, Stronger' (2016), arXiv:1612.08242"
 
-coreml_model.input_description['image'] = 'Input image'
-coreml_model.output_description['grid'] = 'The 13x13 grid with the bounding box data'
+coreml_model.input_description['data'] = 'Input image'
+coreml_model.output_description['model_outputs0'] = 'The 13x13 grid with the bounding box data'
 
 print(coreml_model)
 
