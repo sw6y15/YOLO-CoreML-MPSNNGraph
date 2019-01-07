9f1c9d2c7fb843121a9e68b23512d0fcce5725cf
sw6y15 <sw6y15@soton.ac.uk>
2019-01-07 13:35:36 +0800

    Update Darknet2ONNX.sh

--- Darknet2ONNX.sh
+++ Darknet2ONNX.sh
@@ -5,7 +5,7 @@ rm -rf TinyYOLO-CoreML/TinyYOLO-CoreML/TinyYOLO.mlmodel
 echo "Converting..."
 WRONG_FLAG=1
 cd Convert/yad2k
-if [ ! -f "$1"] || [ ! -f "$2"] || [ -n "$1"] || [ -n "$2"]; then
+if [ ! -f $1] || [ ! -f $2]; then
         echo "$1 not found! Use default config file tiny-yolo-voc.cfg"
 	echo "$2 not found! Use default weights file tiny-yolo-voc.weights"
 	"python3" yad2k.py -p ../tiny-yolo-voc.cfg ../tiny-yolo-voc.weights model_data/tiny-yolo-voc.h5 
